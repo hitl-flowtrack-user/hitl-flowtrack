bb2817d683c7968e5604df0f10454d6897007131
hitl-flowtrack-user <hadiemart@gmail.com>
2026-02-17 21:15:22 +0500

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your exact web app's Firebase configuration from console
 const firebaseConfig = {
   apiKey: "AIzaSyDlzaGO8koBDOYvCdSglZjlvT8_UZCA9XI",
   authDomain: "elite-vault-93de5.firebaseapp.com",
@@ -12,9 +11,6 @@ const firebaseConfig = {
   appId: "1:745842974621:web:f63f46dc7d0938f3bb9974"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export services for use in other files
 export const auth = getAuth(app);
 export const db = getFirestore(app);

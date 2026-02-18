64a1121cec61d9c861cdf0d3635ec751c73aae9c
hitl-flowtrack-user <hadiemart@gmail.com>
2026-02-18 20:04:43 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,3 +9,15 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './app'; // 'A' ko 'a' kar diya hai taake file mil jaye
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

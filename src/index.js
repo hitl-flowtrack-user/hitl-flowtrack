af20218dca046242d88f5c6798fd67740b65a57a
hitl-flowtrack-user <hadiemart@gmail.com>
2026-02-18 20:04:56 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './app'; // Ensure yahan 'app' small letters mein ho kyunke file 'app.js' hai
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './app'; // 'A' ko 'a' kar diya hai taake file mil jaye
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

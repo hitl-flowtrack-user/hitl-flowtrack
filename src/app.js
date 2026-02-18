ad164a3f2ba9c0317d63cd53699d332e52d8bcec
hitl-flowtrack-user <hadiemart@gmail.com>
2026-02-18 19:42:32 +0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/navbar';
+import AddItem from './components/additem';
+import InventoryView from './components/inventoryview';
+import Login from './components/login';
+
+function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("flowtrack_session") === "active");
+
+  return (
+    <Router>
+      {isAuth && <Navbar setAuth={setIsAuth} />}
+      <Routes>
+        <Route path="/login" element={<Login setAuth={setIsAuth} />} />
+        <Route path="/additem" element={isAuth ? <AddItem /> : <Navigate to="/login" />} />
+        <Route path="/inventoryview" element={isAuth ? <InventoryView /> : <Navigate to="/login" />} />
+        <Route path="/" element={isAuth ? <div style={{textAlign:'center', color:'#fff', marginTop:'50px'}}><h1>SYSTEM ONLINE</h1></div> : <Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;

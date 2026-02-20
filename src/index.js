7abc134b2dadf36e5e927da4e531a21356d00816
hitl-flowtrack-user <hadiemart@gmail.com>
2026-02-20 19:55:53 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App'; // App ab components ke andar hai
+import './index.css'; // Agar aapke paas index.css hai to yeh line rehne dein
+import App from './components/App'; // YEH LINE ZAROORI HAI: App ab components folder ke andar hai
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

c14aa3ebb434ec4d61591d10657b23750b56a1c4
hitl-flowtrack-user <hadiemart@gmail.com>
2026-02-22 00:03:56 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -14,3 +14,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
+// --- Ye line Offline Support enable karti hai ---
+enableIndexedDbPersistence(db).catch((err) => {
+    if (err.code === 'failed-precondition') {
+        console.log("Multiple tabs open, persistence can only be enabled in one tab at a time.");
+    } else if (err.code === 'unimplemented') {
+        console.log("The current browser does not support all of the features needed to enable persistence");
+    }
+});

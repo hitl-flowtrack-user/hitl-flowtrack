992b996ae5fc07cd028d1e23e297b085e22bc0a9
hitl-flowtrack-user <hadiemart@gmail.com>
2026-02-21 21:32:39 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+// ⚠️ Sab se pehle Firebase Console se naye details copy karein
 const firebaseConfig = {
-  // Console se copy karte waqt dhyan rakhen ke koi space na aaye
-  apiKey: "AIzaSy........................", 
+  apiKey: "AIzaSy........................", // Yahan apni API Key bina kisi space ke likhen
   authDomain: "hitl-flowtrack.firebaseapp.com",
   projectId: "hitl-flowtrack",
   storageBucket: "hitl-flowtrack.appspot.com",
-  messagingSenderId: "367341951508",
-  appId: "1:367341951508:web:abcdef123456"
+  messagingSenderId: "367341951508", 
+  appId: "1:367341951508:web:abcdef123456" 
 };
 
 // Initialize Firebase

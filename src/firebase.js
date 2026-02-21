14db39680e927c6a5a945876e08b96fbe1f8e90c
hitl-flowtrack-user <hadiemart@gmail.com>
2026-02-21 22:58:49 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,10 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlzaGO8koBDOYvCdSglZjlvT8_UZCA9XI", // Apni sahi API key yahan rakhen
-  authDomain: "elite-vault-93de5.firebaseapp.com",
-  projectId: "elite-vault-93de5", // Check karein agar ye HITL-FlowTrack ho gaya hai
-  storageBucket: "elite-vault-93de5.firebasestorage.app",
+  apiKey: "AIzaSyDlzaGO8koBDOYvCdSglZjlvT8_UZCA9XI",
+  authDomain: "hitl-flowtrack.firebaseapp.com",
+  projectId: "hitl-flowtrack",
+  storageBucket: "hitl-flowtrack.firebasestorage.app",
   messagingSenderId: "745842974621",
   appId: "1:745842974621:web:f63f46dc7d0938f3bb9974"
 };
